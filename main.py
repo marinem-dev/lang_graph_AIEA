a4db941072af39ec888bdfaa3a1f2a5c633097a2
Marine Martinez <dmart184@ucsc.edu>
2025-06-12 17:24:03 -0700

    main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+import os
+from dotenv import load_dotenv
+from langgraph.prebuilt import create_react_agent
+
+load_dotenv()
+
+api_key = os.getenv("ANTHROPIC_API_KEY")
+
+print(f"API Key: {api_key}")
+
+def get_weather(city: str) -> str:
+    """Get weather for a given city."""
+    return f"It's always sunny in {city}!"
+
+agent = create_react_agent(
+    model="anthropic:claude-3-7-sonnet-latest",
+    tools=[get_weather],
+    prompt="You are a helpful assistant"
+)
+
+# Run the agent
+agent.invoke(
+    {"messages": [{"role": "user", "content": "what is the weather in sf"}]}
+)
+
